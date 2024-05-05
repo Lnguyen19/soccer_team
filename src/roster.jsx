273ccf86be003d91bcf96b1c03b7f5920231676a
lam <72236208+Lnguyen19@users.noreply.github.com>
2024-05-04 20:54:12 -0700

    h

--- src/roster.jsx
+++ src/roster.jsx
@@ -3,59 +3,71 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import "./roster.css";
 import Navigation from "./navigation";
+const Roster = ()=>{
+	const [roster,setRoster] = useState([]);
+useEffect(()=>{
+	axios.get("https://soccerteam-953874d541a4.herokuapp.com/getRoster", {withCredentials:true}).then((response)=>{
+		if(response.data){
+			console.log("got the data")
+			const sorted = response.data.sort((a,b)=>a._id - b._id);
+			setRoster(sorted);
+      
+}
+		else{
+			console.log("something is wrong");
+		}
+	}).catch(error=>{
+		console.log(error);
+	});
 
-const Roster = () => {
-  const [roster, setRoster] = useState([]);
+},[]);
 
-  useEffect(() => {
-    axios.get("https://soccerteam-953874d541a4.herokuapp.com/getRoster", { withCredentials: true })
-      .then(response => {
-        if (response.data) {
-          const sorted = response.data.sort((a, b) => a._id - b._id);
-          setRoster(sorted);
-        } else {
-          console.log("something is wrong");
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+const picture = (picture)=>{
+	if(picture==="na"){
+		return "https://i.pinimg.com/736x/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg";
+	}
+	else{
+		return picture;
+	}
 
-  const picture = (picture) => {
-    if (picture === "na") {
-      return "https://i.pinimg.com/736x/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg";
-    } else {
-      return picture;
-    }
-  }
+}
+return(<>
+	<Navigation/>
+ <div className="standing-table">
+<table>
+  <tr>
+    <th>Photo</th>
+    <th>Name</th>
+    <th>Kit Number</th>
+    <th>Goal(s)</th>
+    <th>Yellow Card</th>
+    <th>Red Card</th>
+    <th>Games Played</th>
+  </tr>
 
-  return (
-    <>
-      <Navigation />
-      <div className="standing-table">
-        {roster.length === 0 ? (
-          <div>
-            <h1>No players listed</h1>
-          </div>
-        ) : (
-          roster.map((player, index) => (
-            <div className="player-card" key={index}>
-              <img className="playerImage" src={picture(player.Picture)} alt={player.Name} />
-              <div className="player-info">
-                <h3>{player.Name}</h3>
-                <p>Kit Number: {player.Number}</p>
-                <p>Goal(s): {player.Goal}</p>
-                <p>Yellow Card: {player.YellowCard}</p>
-                <p>Red Card: {player.RedCard}</p>
-                <p>Games Played: {player.GamesPlayed}</p>
-              </div>
-            </div>
-          ))
-        )}
-      </div>
-    </>
-  );
+{ roster.length===0?(
+<div> 
+<h1>No player listed</h1>
+</div>
+	):roster.map((rosters,index)=>(
+     <tr key = {index}>
+    <td><img  class = "playerImage"src = {picture(rosters.Picture) }/></td>
+    <td>{rosters.Name}</td>
+    <td>{rosters.Number}</td>
+    <td>{rosters.Goal}</td>
+    <td>{rosters.YellowCard}</td>
+     <td>{rosters.RedCard}</td>
+    <td>{rosters.GamesPlayed}</td>
+  </tr>
+))
+
+
 }
 
+</table>
+</div>
+
+	</>)
+
+}
 export default Roster;
